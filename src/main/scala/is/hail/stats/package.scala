9eef779cdfe1a957f5e875375c7954020cacb4c3
Zach Zappala <zach@MacArthur-ZappalaMBP.local>
2017-11-06 14:32:11 -0500

    Added code to calculate filtering frequencies

--- src/main/scala/is/hail/stats/package.scala
+++ src/main/scala/is/hail/stats/package.scala
@@ -110,6 +110,31 @@ package object stats {
     }
   }
 
+  def findMaxAC(af:Double, an:Int, ci:Double = .95):Int = {
+   if (af == 0) {
+      0
+    } else {
+      val quantile_limit = ci // ci for one-sided, 1-(1-ci)/2 for two-sided
+      val max_ac = qpois(quantile_limit,an*af)
+      max_ac
+    }
+  }
+
+  def calcFreqFilter(ac: Int, an: Int, ci: Double = .95, lower: Double = .1/(2*60706),  upper:Double = 2, tol:Double = 1e-7, precision:Double = 1e-6): Double = {
+    if (ac <= 0 || an == 0) {
+      0.0
+    } else {
+      var f = (af: Double) => ac.toDouble - 1 - qpois(ci, an.toDouble * af)
+      val root = uniroot(f, lower, upper, tol)
+      val rounder = 1d/(precision/100d)
+      var max_af = math.round(root.getOrElse(0.0) * rounder) / rounder
+      while(findMaxAC(max_af, an) < ac) {
+        max_af += precision
+      }
+      max_af - precision
+    }
+  }
+
   def FisherExactTest(a: Int, b: Int, c: Int, d: Int,
     oddsRatio: Double = 1d, confidence_level: Double = 0.95,
     alternative: String = "two.sided"): Array[Option[Double]] = {
